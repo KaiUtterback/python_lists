4c32a3ccf7e4e38eedb68f42b0f2bda31fd030d2
KaiUtterback <156825843+KaiUtterback@users.noreply.github.com>
2024-02-10 16:49:04 -0600

    First Commit

---
+++ python_list_assignment.py
@@ -0,0 +1,7 @@
+# Question 1 :Python List Transformation
+# Task set 1: Grades: Set the grades in decending order and display the sorted list:
+print(" ")
+
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+grades.sort()
+print(grades)
