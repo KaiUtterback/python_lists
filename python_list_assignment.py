0ba2e68d2f4a5096deae39676c5c1f477e7316a8
KaiUtterback <156825843+KaiUtterback@users.noreply.github.com>
2024-02-10 17:23:19 -0600

    Update python_list_assignment.py

--- python_list_assignment.py
+++ python_list_assignment.py
@@ -4,4 +4,28 @@ print(" ")
 
 grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
 grades.sort()
+grades.reverse()
 print(grades)
+
+# Task 2: Find the average
+
+average = sum(grades) // len(grades)
+print("The average grade is,", average)
+
+# Task 3: Replace any grade below 80 with the value Failed
+
+grades = ["Failed" if grade < 80 else grade for grade in grades]
+
+# Question 2: Advanced List Methods and Identity Operators:
+
+print(" ")
+
+# Task 1: Find out which students both submitted their assignments and attended the class
+
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"]
+
+if submitted is attended:
+    print("all students attended class and submitted assignments")
+else:
+    print("Not all students attended class and submitted assignments")
